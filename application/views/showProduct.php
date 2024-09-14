1358f076120dc11e6feb2f1d26df397bb32cddaf
JeyZ9 <ggjj4511@gmail.com>
2024-09-14 21:39:31 +0700

    add table

--- application/views/showProduct.php
+++ application/views/showProduct.php
@@ -1,5 +1,15 @@
 <?php
-    foreach($query as $row){
-        echo $row->id . " " . $row->code . " " . $row->name . " " . $row->price . " ";
-        echo "<br>";
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Code</th><th>Name</th><th>Price</th></tr>";
+    
+    foreach($query as $row) {
+        echo "<tr>";
+        echo "<td>" . $row->id . "</td>";
+        echo "<td>" . $row->code . "</td>";
+        echo "<td>" . $row->name . "</td>";
+        echo "<td>" . $row->price . "</td>";
+        echo "</tr>";
     }
+    
+    echo "</table>";
+    
